7929d634bea0a7501ccff58b5169552b7811909d
Trường <38981352+hinatosss111@users.noreply.github.com>
2019-03-12 18:28:09 +0700

    Add files via upload

--- src/org/buffalocoder/quanlybangdia/views/MainForm.java
+++ src/org/buffalocoder/quanlybangdia/views/MainForm.java
@@ -1,21 +1,37 @@
 package org.buffalocoder.quanlybangdia.views;
 
-import org.buffalocoder.quanlybangdia.utils.Values;
+import java.awt.BorderLayout;
 
 import javax.swing.*;
-import java.awt.*;
+
+import org.buffalocoder.quanlybangdia.components.UIComponent;
 
 public class MainForm extends JFrame {
     //========== CONSTANT ==========//
-    final int WIDTH = 800;
+    final int WIDTH = 1200;
     final int HEIGHT = 600;
     final String TITLE = "Quản lý băng đĩa";
-
+    //========= UI ===========//
+    private JPanel mainPanel, menuPanel, contentPanel;
+    private JTabbedPane menuTabbed;
+    private JTextField txtSearch;
+    
+    
 
     /**
      * Tạo GUI
      */
     private void prepareUI(){
+    	this.setContentPane(mainPanel = new JPanel(new BorderLayout()));
+//    				header
+    	UIComponent mnbar = new UIComponent();
+    	setJMenuBar(mnbar.Menu());
+//    	            content
+        contentPanel = new JPanel();
+        contentPanel.add(mnbar.btn("Thêm"));
+        contentPanel.add(mnbar.btn("Sửa"));
+        contentPanel.add(mnbar.btn("Xóa"));
+        mainPanel.add(contentPanel, BorderLayout.CENTER);
 
     }
 
@@ -27,10 +43,11 @@ public class MainForm extends JFrame {
             @Override
             public void run() {
                 prepareUI();
-
                 setSize(WIDTH, HEIGHT);
                 setDefaultCloseOperation(EXIT_ON_CLOSE);
+                setLocation(300, 100);
                 setTitle(TITLE);
+                setUndecorated(true);
                 setVisible(true);
             }
         });
