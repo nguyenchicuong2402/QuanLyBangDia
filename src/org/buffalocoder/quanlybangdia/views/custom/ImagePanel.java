6031b99ec76cbd05e45dec958ba0007782f13427
nguyenchicuong2402 <nguyenchicuong2402@gmail.com>
2019-04-23 22:56:10 +0700

    fix image trang chủ

--- src/org/buffalocoder/quanlybangdia/views/custom/ImagePanel.java
+++ src/org/buffalocoder/quanlybangdia/views/custom/ImagePanel.java
@@ -28,7 +28,7 @@ public class ImagePanel extends JPanel {
     @Override
     protected void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(image, 0, 0, this); // see javadoc for more info on the parameters
+        g.drawImage(image, 130, 140, this);
     }
 
 }
